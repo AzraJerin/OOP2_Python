b3a466ed5490d6555e27515da9c00a39ac1ff683
Azra Jerin <aushajerin@gmail.com>
2024-08-28 12:33:59 -0700

    updating the lectureCode file

--- LabTask_1/lectureCode.py
+++ LabTask_1/lectureCode.py
@@ -43,3 +43,66 @@ print(sub)
 print(mul) 
 print(mod) 
 print(p)
+
+a = True
+b = False
+print(a and b) 
+print(a or b) 
+print(not a)
+
+a = 10
+b = 4
+print(a & b) 
+print(a | b) 
+print(~a) 
+print(a ^ b) 
+print(a >> 2) 
+print(a << 2) 
+
+a = 10
+b = a 
+print(b) 
+b += a 
+print(b) 
+b -= a 
+print(b) 
+b *= a 
+print(b) 
+b <<= a 
+print(b)
+i = 20
+if (i < 15): 
+    print("i is smaller than 15") 
+    print("i'm in if Block") 
+else: 
+    print("i is greater than 15") 
+    print("i'm in else Block") 
+print("i'm not in if and not in else Block") 
+
+i = 20
+if (i == 10): 
+    print("i is 10") 
+elif (i == 15): 
+    print("i is 15") 
+elif (i == 20): 
+    print("i is 20") 
+else: 
+    print("i is not present") 
+for i in range(0, 10, 2): 
+    print(i) 
+
+    count = 0
+while (count < 3): 
+    count = count + 1
+    print("Hello Geek")
+
+    def evenOdd():
+      if(x % 2 == 0):
+        print("even")
+      else:
+        print("odd")
+
+
+# Driver code to call the function
+evenOdd(2)
+evenOdd(3)
