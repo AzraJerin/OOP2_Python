63ad4a63e1b03a6679114c1fe5c777d78e918ba1
Azra Jerin <aushajerin@gmail.com>
2024-08-28 12:20:30 -0700

    Upload my labtask_1

---
+++ LabTask_1/lectureCode.py
@@ -0,0 +1,45 @@
+#printing
+print("Hello World! I Don't Give a Bug")
+
+# sample comment 
+# This is Python Comment
+name = "geeksforgeeks"
+print(name) 
+
+# An integer assignment
+age = 45
+# A floating point
+salary = 1456.8
+# A string
+name = "John"
+print(age)
+print(salary)
+print(name)
+
+x = "Hello World" # string
+x = 50  # integer
+x = 60.5  # float
+x = 3j  # complex
+x = ["geeks", "for", "geeks"]  # list 
+x = ("geeks", "for", "geeks")  # tuple
+x = {"name": "Suraj", "age": 24} # dict
+x = {"geeks", "for", "geeks"} # set
+x = True  # bool
+x = b"Geeks" # binary
+
+# Python program show input and Output
+val = input("Enter your value: ") 
+print(val) 
+
+a = 9
+b = 4
+add = a + b 
+sub = a - b 
+mul = a * b 
+mod = a % b 
+p = a ** b 
+print(add) 
+print(sub) 
+print(mul) 
+print(mod) 
+print(p)
